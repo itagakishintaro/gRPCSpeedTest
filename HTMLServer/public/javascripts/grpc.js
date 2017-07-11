c2bf2368a8bb17e7610e4c5d35a79f0cabca753d
itagaki <itagakishintaro@jp.fujitsu.com>
2017-07-11 12:31:38 +0900

    delete HTMLServer

--- HTMLServer/public/javascripts/grpc.js
+++
@@ -1,9 +0,0 @@
-$( '#grpc' ).on( 'click', () => {
-    $.get( 'api/grpc' ).done( ( data ) => {
-        $( '#grpcTime' ).text( data.time );
-        for ( let i = 0; i <= 9; i++ ) {
-            $( '#grpcImg' + i ).attr( 'src', 'data:image/png;base64,' + data[ 'b64image' + i ] );
-        }
-    } );
-
-} );
--- HTMLServer/public/javascripts/rest.js
+++
@@ -1,8 +0,0 @@
-$( '#rest' ).on( 'click', () => {
-    $.get( 'api/rest' ).done( ( data ) => {
-        $( '#restTime' ).text( data.time );
-        for ( let i = 0; i <= 9; i++ ) {
-            $( '#restImg' + i ).attr( 'src', 'data:image/png;base64,' + data[ 'b64image' + i ] );
-        }
-    } );
-} );
